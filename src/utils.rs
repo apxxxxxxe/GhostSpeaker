20c34ead95bc136caad42bb7f4685e32f0c841e1
apxxxxxxe <calcium629@gmail.com>
2023-10-31 07:34:52 +0900

    update: check_connection

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 pub async fn check_connection(port: i32) -> bool {
-    let url = format!("http://127.0.0.1:{}/docs", port);
+    let url = format!("http://127.0.0.1:{}", port);
     reqwest::Client::new()
         .get(url)
         .timeout(Duration::from_millis(500))
