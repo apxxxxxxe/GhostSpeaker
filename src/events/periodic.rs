7043caadd290134023bdd16d24b93feae99b1723
apxxxxxxe <calcium629@gmail.com>
2025-09-11 01:34:50 +0900

    起動時の更新チェック&実行を復元
    This reverts commit b07e2879fff095306fbefce5cda61080134c9fdc.

--- src/events/periodic.rs
+++ src/events/periodic.rs
@@ -2,10 +2,11 @@ use crate::events::common::*;
 use crate::plugin::request::PluginRequest;
 use crate::plugin::response::PluginResponse;
 use crate::queue::CONNECTION_DIALOGS;
-use crate::variables::LOG_INIT_SUCCESS;
+use crate::variables::{LOG_INIT_SUCCESS, PLUGIN_NAME};
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
+static UPDATE_CHECKED: Lazy<Mutex<bool>> = Lazy::new(|| Mutex::new(false));
 static LOG_INIT_CHECKED: Lazy<Mutex<bool>> = Lazy::new(|| Mutex::new(false));
 
 pub(crate) fn on_second_change(_req: &PluginRequest) -> PluginResponse {
@@ -14,6 +15,18 @@ pub(crate) fn on_second_change(_req: &PluginRequest) -> PluginResponse {
     if let Some(line) = dialogs.pop() {
       lines.push(line);
     }
+  }
+
+  let update: String;
+  let update_checked;
+  {
+    update_checked = *UPDATE_CHECKED.lock().unwrap();
+  }
+  if !update_checked {
+    *UPDATE_CHECKED.lock().unwrap() = true;
+    update = format!("\\C\\![updateother,--plugin={}]", PLUGIN_NAME);
+  } else {
+    update = String::new();
   }
 
   let log_init_checked;
@@ -46,11 +59,14 @@ pub(crate) fn on_second_change(_req: &PluginRequest) -> PluginResponse {
   if !lines.is_empty() {
     new_response_with_nobreak(
       format!(
-        "\\C\\![set,trayballoon,--text={},--title=GhostSpeaker,--icon=info,--timeout=3]",
+        "\\C\\![set,trayballoon,--text={},--title=GhostSpeaker,--icon=info,--timeout=3]{}",
         lines.join(" / "),
+        update
       ),
       false,
     )
+  } else if !update.is_empty() {
+    new_response_with_script(update, false)
   } else {
     new_response_nocontent()
   }
